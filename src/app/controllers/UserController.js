709f74dcf56ddfb1ec0ccd3b447a1ce890261ee2
sonTung Nguyen <nguyensontungtdn1901@gmail.com>
2024-08-22 01:09:11 +0700

    add: implement functions handling the authenticating and storing user data

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,4 +1,14 @@
+const User = require("../models/User");
+const { mongooseToObject } = require("../../utils/mongoose");
+
 class UserController {
+  create(req, res, next) {
+    res.render("sign-up", {
+      title: "Sign Up",
+      styles: ["sign-up.css"],
+    });
+  }
+
   entry(req, res, next) {
     res.render("sign-in", {
       title: "Sign In",
@@ -6,11 +16,43 @@ class UserController {
     });
   }
 
-  create(req, res, next) {
-    res.render("sign-up", {
-      title: "Sign Up",
-      styles: ["sign-up.css"],
-    });
+  async auth(req, res, next) {
+    try {
+      const matchedUser = await User.findOne({ email: req.body.email });
+      if (mongooseToObject(matchedUser).password === req.body.password) {
+        res.render("home");
+      } else {
+        res.send("Wrong Password");
+      }
+    } catch (error) {
+      res.send("Wrong details");
+    }
+  }
+
+  async store(req, res, next) {
+    const data = {
+      email: req.body.email,
+      phoneNumber: req.body.phoneNumber,
+      password: req.body.password,
+      profileImg: req.body.profileImg,
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      address: req.body.address,
+      city: req.body.city,
+      zipCode: req.body.zipCode,
+      country: req.body.country,
+      accountType: req.body.accountType,
+      schoolName:
+        req.body.accountType === "instructor" ? req.body.schoolName : undefined,
+      jobTitle:
+        req.body.accountType === "instructor" ? req.body.jobTitle : undefined,
+      specialization:
+        req.body.accountType === "instructor"
+          ? req.body.specialization
+          : undefined,
+    };
+    await User.insertMany([data]);
+    res.redirect("/");
   }
 }
 
--- src/routes/user.js
+++ src/routes/user.js
@@ -6,7 +6,13 @@ const userController = require("../app/controllers/UserController");
 // [GET] /sign-in
 router.get("/sign-in", userController.entry);
 
+// [POST] /sign-in
+router.post("/sign-in", userController.auth);
+
 // [GET] /sign-up
 router.get("/sign-up", userController.create);
+
+// [POST] /sign-up
+router.post("/sign-up", userController.store);
 
 module.exports = router;
