4ecbb44d4bdf297f3b3d7b442f8efc1441c8b47a
sonTung Nguyen <nguyensontungtdn1901@gmail.com>
2024-08-22 01:20:28 +0700

    chore: remove the commenting code

--- src/index.js
+++ src/index.js
@@ -4,10 +4,7 @@ const path = require("path");
 const app = express();
 const port = 3000;
 
-// const hbs = require("hbs");
-// const collection = require("./mongodb");
-// const { profile } = require("console");
-
+const route = require("./routes/index");
 const db = require("./config/db");
 
 // STATIC FILES
@@ -15,7 +12,8 @@ app.use(express.static(path.join(__dirname, "/public")));
 
 // For parsing application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
-// For parsing application/json
+
+// APPLICATION/JSON PARSING
 app.use(express.json());
 
 // EXPRESS HANDLEBARS CONFIGURATION
@@ -26,53 +24,8 @@ app.set("views", path.join(__dirname, "/resources/views"));
 // DATABASE CONNECTION
 db.connect();
 
-app.get("/", (req, res) => {
-  res.render("login");
-});
-
-app.get("/signup", (req, res) => {
-  res.render("signup");
-});
-
-app.post("/signup", async (req, res) => {
-  const data = {
-    email: req.body.email,
-    phoneNumber: req.body.phoneNumber,
-    password: req.body.password,
-    profileImg: req.body.profileImg,
-    firstName: req.body.firstName,
-    lastName: req.body.lastName,
-    address: req.body.address,
-    city: req.body.city,
-    zipCode: req.body.zipCode,
-    country: req.body.country,
-    accountType: req.body.accountType,
-    schoolName:
-      req.body.accountType === "instructor" ? req.body.schoolName : undefined,
-    jobTitle:
-      req.body.accountType === "instructor" ? req.body.jobTitle : undefined,
-    specialization:
-      req.body.accountType === "instructor"
-        ? req.body.specialization
-        : undefined,
-  };
-  await collection.insertMany([data]);
-  res.render("home");
-});
-
-app.post("/login", async (req, res) => {
-  try {
-    const check = await collection.findOne({ email: req.body.email });
-
-    if (check.password === req.body.password) {
-      res.render("home");
-    } else {
-      res.send("Wrong Password");
-    }
-  } catch (error) {
-    res.send("Wrong details");
-  }
-});
+// APPLICATION ROUTER from src/routes
+route(app);
 
 app.listen(port, () => {
   console.log(`Server starts on http://localhost:${port}/`);
