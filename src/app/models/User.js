bedf798bc14d91d21708f9321639897604d7c648
sonTung Nguyen <nguyensontungtdn1901@gmail.com>
2024-08-30 19:40:05 +0700

    add: handle validation in `User` modal and change middleware folder name

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,16 +1,30 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const { emailPattern, phonePattern } = require("../../utils/pattern");
 
 const User = new Schema({
-  email: { type: String, unique: true, required: true },
-  phone: { type: String, required: true },
-  password: { type: String, required: true },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    validate: (value) => emailPattern.test(value),
+  },
+
+  phone: {
+    type: String,
+    required: true,
+    validate: (value) => phonePattern.test(value.replace(/\D/g, "")),
+  },
+
+  password: { type: String, required: true, minLength: 3, maxLength: 15 },
+
   profileImg: { type: String, default: "none" },
-  firstName: { type: String, required: true, length: 15 },
-  lastName: { type: String, required: true, length: 15 },
-  address: { type: String, required: true },
-  city: { type: String, required: true },
-  zipcode: { type: String, required: true },
+
+  firstName: { type: String, required: true, minlength: 2, maxlength: 15 },
+  lastName: { type: String, required: true, minlength: 2, maxlength: 15 },
+  address: { type: String, required: true, maxlength: 255 },
+  city: { type: String, required: true, maxlength: 20 },
+  zipcode: { type: String, required: true, minLength: 4, maxLength: 6 },
   country: { type: String, required: true },
 
   accountType: {
@@ -21,14 +35,23 @@ const User = new Schema({
 
   schoolName: {
     type: String,
-    required: () => this.accountType === "INSTRUCTOR",
+    required: function () {
+      return this.accountType === "INSTRUCTOR";
+    },
   },
 
-  jobTitle: { type: String, required: () => this.accountType === "INSTRUCTOR" },
+  jobTitle: {
+    type: String,
+    required: function () {
+      return this.accountType === "INSTRUCTOR";
+    },
+  },
 
   specialization: {
     type: [String],
-    required: () => this.accountType === "INSTRUCTOR",
+    required: function () {
+      return this.accountType === "INSTRUCTOR";
+    },
   },
 });
 
--- src/routes/user.js
+++ src/routes/user.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const userController = require("../app/controllers/UserController");
-const { checkExistedEmail, isValidAuth } = require("../middlewares/validate");
+const { checkExistedEmail, isValidAuth } = require("../middlewares/isExisted");
 const { authenticate } = require("../middlewares/authentication");
 
 router.get("/sign-up", userController.create);
