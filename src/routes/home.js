d6c794fec4575bf7e211c583658185ace8815a80
Ha <s3981134@rmit.edu.vn>
2024-09-01 22:17:53 +0700

    feat: navbar homepage content

--- src/routes/home.js
+++ src/routes/home.js
@@ -6,4 +6,9 @@ const homeController = require("../app/controllers/homeController");
 // [GET] /sign-in
 router.get("/", homeController.show);
 
+// About us page, Pricing page, FAQ page, Contact us page, etc.
+router.get("/about-us", HomeRender.aboutUs);
+router.get("/pricing", HomeRender.pricing);
+router.get("/faq", HomeRender.faq);
+
 module.exports = router;
