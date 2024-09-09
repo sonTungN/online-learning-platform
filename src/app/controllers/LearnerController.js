83d768d6c1cd83617fce05c7ff48b482d8376488
sonTung Nguyen <nguyensontungtdn1901@gmail.com>
2024-09-10 00:52:48 +0700

    add: implement learners views `purchased`, `on-trial`, `wishlist` to store courses, and implement basic routes and controllers

--- src/app/controllers/LearnerController.js
+++ src/app/controllers/LearnerController.js
@@ -10,6 +10,39 @@ class LearnerController {
       next(e);
     }
   }
+
+  viewPurchased(req, res, next) {
+    try {
+      res.render("learner/view-courses", {
+        title: "My learning",
+        styles: ["learner/view-courses.css", "bootstrap_v5.css"],
+      });
+    } catch (e) {
+      next(e);
+    }
+  }
+
+  viewTrial(req, res, next) {
+    try {
+      res.render("learner/on-trial", {
+        title: "My learning",
+        styles: ["learner/on-trial.css", "bootstrap_v5.css"],
+      });
+    } catch (e) {
+      next(e);
+    }
+  }
+
+  viewWishlist(req, res, next) {
+    try {
+      res.render("learner/wishlist", {
+        title: "My learning",
+        styles: ["learner/wishlist.css", "bootstrap_v5.css"],
+      });
+    } catch (e) {
+      next(e);
+    }
+  }
 }
 
 module.exports = new LearnerController();
--- src/routes/learner.js
+++ src/routes/learner.js
@@ -5,4 +5,10 @@ const learnerController = require("../app/controllers/LearnerController");
 
 router.get("/order-placement", learnerController.order);
 
+router.get("/my-courses/purchased", learnerController.viewPurchased);
+
+router.get("/my-courses/trial", learnerController.viewTrial);
+
+router.get("/my-courses/wishlist", learnerController.viewWishlist);
+
 module.exports = router;
